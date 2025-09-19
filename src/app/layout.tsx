db00d230ec6cb0ef996d30ff67f8b13003563b87
Muhammad-Umar125 <umerq0875@gmail.com>
2025-09-19 19:15:46 +0500

    added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({ 
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,8 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
         {children}
       </body>
     </html>
